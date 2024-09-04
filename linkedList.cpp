dbb4751713fe7134747876a322c8d40bbd203486
ndeyoung0621 <145629761+ndeyoung0621@users.noreply.github.com>
2024-09-03 18:16:23 -0700

    created .hpp file, header for .cpp file

---
+++ linkedList.cpp
@@ -0,0 +1,4 @@
+#include "linkedList.hpp"
+#include <iostream>
+
+using namespace std;
---
+++ linkedList.hpp
@@ -0,0 +1,27 @@
+#ifndef linkedList_h
+#define linkedList_ho
+
+struct Node  {
+//Defining a "node", a node has data (trees), a pointer to the next node and a pointer to the previous node
+
+    int data;
+    Node* prev;
+    Node* next;
+
+    Node(int val) : data(val), prev(nullptr), next(nullptr) {}
+
+};
+
+class linkedList {
+//defines 
+    private:
+        Node* head;
+        Node* tail;
+    public:
+
+    void insert (int value);
+    void printList () const;
+
+};
+
+#endif
